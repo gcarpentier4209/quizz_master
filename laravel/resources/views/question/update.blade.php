07b43fcdac4ac5ddbe51e434e700e1b70fb571bf
yannik <yguillodo@gmail.com>
2019-05-24 13:48:47 +0200

    example context

--- laravel/resources/views/question/update.blade.php
+++ laravel/resources/views/question/update.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Question</div>
+                    <div class="card-header">Création d'une nouvelle question</div>
 
                     <div class="card-body">
                         @if (session('status'))
@@ -14,28 +14,63 @@
                             </div>
                         @endif
 
-                       UPDATE QUESTIONS ...
+                        <form action="{{route("create.question.post")}}" method="post">
+                            @csrf
+                            <div class="row">
+                                <label for="body">Question
+                                    <input type="text" name="body" value="{{$question->body}}>
+                                </label>
+                            </div>
+
+                            <div class="row">
+                                <label for="clue">Indice
+                                    <input type="text" name="clue" value="{{$question->body}}>
+                                </label>
+                            </div>
+
+                            <div class="row">
+                                <label for="difficulty">Difficulté
+                                    <input type="text" name="difficulty" value="{{$question->body}}>
+                                </label>
+                            </div>
+
+                            <div class="row">
+                                <label for="solution">Solution
+                                    <input type="text" name="solution" value="{{$question->body}}">
+                                </label>
+                            </div>
+
+                            <div class="row">
+
+                                <label for="answer1">Mauvaise réponse n°1
+                                    <input type="text" name="answer1" value="{{$question->body}}>
+                                </label>
+                            </div>
+
+                            <div class="row">
+
+                                <label for="answer2">Mauvaise réponse n°2
+                                    <input type="text" name="answer2" value="{{$question->body}}>
+                                </label>
+                            </div>
+
+                            <div class="row">
+
+                                <label for="answer3">Mauvaise réponse n°3
+                                    <input type="text" name="answer3" value="{{$question->body}}>
+                                </label>
+                            </div>
+
+                            <div class="row">
+
+                                <input type="submit" value="Création" value="{{$question->body}}>
+                            </div>
 
 
-                        <table>
-                            <thead>
-                            <tr>
-                                <th>Question</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-
-{{--                            {{$question->body}}--}}
-
-                            </tbody>
-                        </table>
-
-
-{{--                        <a href="{{route("create.question")}}">New question</a>--}}
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 @endsection
-
--- laravel/vendor/bin/features/bootstrap/FeatureContext.php
+++ laravel/vendor/bin/features/bootstrap/FeatureContext.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Foundation\Testing\Concerns\MakesHttpRequests;
+use App\Models\Question;
 use App\Models\Topics;
 use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
@@ -9,6 +11,10 @@ use Behat\Gherkin\Node\TableNode;
 /**
  * Defines application features from the specific context.
  */
+
+
+
+
 class FeatureContext implements Context
 {
     /**
@@ -18,8 +24,155 @@ class FeatureContext implements Context
      * You can also pass arbitrary arguments to the
      * context constructor through behat.yml.
      */
+
+    private $url;
+
+    
     public function __construct()
     {
+        $this->url='/questions/new';
+    }
+
+ 
+
+    /**
+     * @Given I am logged in as User
+     */
+    public function iAmLoggedInAsUser()
+    {
+        return true;
+    }
+
+    /**
+     * @When I press :arg1
+     */
+    public function iPress($url)
+    {
+        //$this->getSession()->visit($this->localPath('/questions/new'));
+        $this->get('/questions/new');
+
+    }
+
+    /**
+     * @Then I should see :arg1
+     */
+    public function iShouldSee($status)
+    {
+        $this->iPress('/questions/new')->assertStatus(200);
+
+//        $response->assertStatus(200);
+    }
+
+    /**
+     * @Given no question
+     */
+    public function noQuestion()
+    {
+        $this->question = new Question();
+    }
+
+    /**
+     * @When I post the body of my question :arg1
+     */
+    public function iPostTheBodyOfMyQuestion($body)
+    {
+        PHPUnit\Framework\Assert::assertsame($body, $this->question->getBody());
+    }
+
+    /**
+     * @When I add an level difficulty
+     */
+    public function iAddAnLevelDifficulty()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I add a soluce
+     */
+    public function iAddASoluce()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I add a :arg1 wrong answers
+     */
+    public function iAddAWrongAnswers($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the question is create
+     */
+    public function theQuestionIsCreate()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given I choose the question
+     */
+    public function iChooseTheQuestion()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I can change the body of my question
+     */
+    public function iCanChangeTheBodyOfMyQuestion()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I can change the level difficulty
+     */
+    public function iCanChangeTheLevelDifficulty()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I can change the soluce
+     */
+    public function iCanChangeTheSoluce()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I can change the wrong answers
+     */
+    public function iCanChangeTheWrongAnswers()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the question can be updated
+     */
+    public function theQuestionCanBeUpdated()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I clik on the button :arg1
+     */
+    public function iClikOnTheButton($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the question is delete in the Database
+     */
+    public function theQuestionIsDeleteInTheDatabase()
+    {
+        throw new PendingException();
     }
 
     /**
@@ -27,10 +180,7 @@ class FeatureContext implements Context
      */
     public function anEmptyTopic()
     {
-
-        $this->topics = new Topics();
-      
-
+        throw new PendingException();
     }
 
     /**
@@ -48,4 +198,5 @@ class FeatureContext implements Context
     {
         throw new PendingException();
     }
+
 }
