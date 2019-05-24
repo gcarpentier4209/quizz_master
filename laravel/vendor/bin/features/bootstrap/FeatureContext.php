83e85236a918bb716bc143833cf1e803ec32e25d
gcarpentier4209 <gcarpentier4209@gmail.com>
2019-05-24 14:05:34 +0200

    mise en place test affichage options

--- laravel/vendor/bin/features/bootstrap/FeatureContext.php
+++ laravel/vendor/bin/features/bootstrap/FeatureContext.php
@@ -5,12 +5,19 @@ use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\Mink\Driver\GoutteDriver;
+
 
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext implements Context
 {
+
+    private $driver;
+    private $session;
+    private $button;
+
     /**
      * Initializes context.
      *
@@ -20,32 +27,42 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
+
+        $this->button = [
+            "options"=>"http://localhost/imie/cours/test/quizz_master/laravel/public/options",
+            ];
+        $this->driver = new GoutteDriver();
+        $this->session = new \Behat\Mink\Session($this->driver);
+
+        $this->session->start();
     }
 
     /**
-     * @Given an empty topic
+     * @Given I am logged in as user
      */
-    public function anEmptyTopic()
+    public function iAmLoggedInAsUser()
     {
-
-        $this->topics = new Topics();
-      
-
+        return true;
     }
 
     /**
-     * @When I add a new topic
+     * @When I press   :arg1
      */
-    public function iAddANewTopic()
+    public function iPress($button)
     {
-        throw new PendingException();
+        $url = $this->button[$button];
+        $this->session->visit($url);
     }
 
     /**
-     * @Then the title topic is not null
+     * @Then I should see :arg1
      */
-    public function theTitleTopicIsNotNull()
+    public function iShouldSee($status)
     {
-        throw new PendingException();
+        $status = 200;
+        PHPUnit\Framework\Assert::assertEquals($this->session->getStatusCode(), $status);
     }
+
+
+
 }
